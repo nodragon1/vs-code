9f2194a1da47eb4be60127d11b85dcd9230e4610
nodragon1 <dksdyddlf123@naver.com>
2024-09-30 16:34:42 +0900

    bms

--- plaintext.c
+++ plaintext.c
@@ -4,6 +4,8 @@ int main()
 {
     printf("hi");
     printf("add");
+    printf("one more line");
+    printf("one more line");
 
     return 0;
 }
--- qwerty.c
+++ qwerty.c
@@ -2,6 +2,6 @@
 
 int main()
 {
-    printf("plain text");
+    printf("sfdfsdfsdf");
     return 0;
 }
